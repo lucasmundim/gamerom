168adf22e2aa17c2f8003f69456a2aaef3f157a6
Lucas Mundim <lucas.mundim@g.globo>
2021-05-01 00:01:06 -0300

    Add update command

--- lib/rom/cli.rb
+++ lib/rom/cli.rb
@@ -2,6 +2,9 @@
 
 require 'rom/platform'
 
+require 'fileutils'
+require 'nokogiri'
+require 'rest-client'
 require 'thor'
 require 'yaml'
 
@@ -17,6 +20,37 @@ module Rom
       puts "listing avaiable platforms..."
       platforms = { platforms: Rom::PLATFORM }
       puts platforms.to_yaml
+    rescue => e
+      puts e.message
+      exit 1
+    end
+
+    desc 'update', 'Update local database'
+    option :platform, :aliases => ['-p'], type: :string, required: true, desc: "Which platform to update", enum: Rom::PLATFORM.keys
+    option :v, type: :boolean, default: false, desc: "Show verbose backtrace"
+    def update
+      puts "updating #{options[:platform]} platform..."
+      games = []
+      letters = ('a'..'z').to_a.unshift("0")
+
+      letters.each do |letter|
+        print "#{letter} "
+        page = Nokogiri::HTML(RestClient.get("https://coolrom.com.au/roms/psx/#{letter}/"))
+        regions = page.css('input.region').map { |i| i["name"] }
+        regions.each do |region|
+          games.append *page.css("div.#{region} a").map { |game|
+            {
+              id: game['href'].split('/')[3].to_i,
+              name: game.text,
+              region: region,
+            }
+          }
+        end
+      end
+      puts
+
+      FileUtils.mkdir_p(File.expand_path("~/.rom/cache"))
+      File.write(File.expand_path("~/.rom/cache/#{options[:platform]}.yml"), games.to_yaml)
     rescue => e
       puts e.message
       exit 1
