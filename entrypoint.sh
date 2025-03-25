04f007c4365c21e4203f37374aa53cd4fe3444af
Lucas Mundim <lucas.mundim@inda.band>
2025-03-25 10:05:03 -0300

    fix: git warning

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 delgroup dialout
 addgroup -g "$GROUP_ID" rungroup
 adduser -u $UID -D -s /bin/sh -G rungroup runuser
+git config --system --add safe.directory '/app'
 command="cd /app && GEM_HOME="/bundle" PATH=$GEM_HOME/bin:$GEM_HOME/gems/bin:$PATH bundle exec ./exe/gamerom ${@}"
 su - runuser -c "$command"
