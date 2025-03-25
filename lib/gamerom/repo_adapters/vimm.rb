9cdd437495f158145db806e4c1a530e5fb1c87a8
Lucas Mundim <lucas.mundim@inda.band>
2025-03-24 21:21:49 -0300

    fix: missing sections for vimm repo

--- lib/gamerom/repo_adapters/vimm.rb
+++ lib/gamerom/repo_adapters/vimm.rb
@@ -43,6 +43,7 @@ module Gamerom
           page = nokogiri_get("https://vimm.net/vault/?p=list&system=#{platform}&section=#{section}")
           game_links = page.css('table.hovertable td:first-child a:first-child')
           yield game_links.map { |game_link| game(game_link) }, index
+        rescue RestClient::NotFound
         end
       end
 
